b33bc801f2b38ec5a91569b425e11949b4f35413
Robert Sesek <rsesek@bluestatic.org>
2009-02-24 08:39:36 -0500

    Split out the common footer code into the _footer view
    * Create new view _footer to hold common footer info
    * Load this view inside detail_view and shout_view rather than duplicating code
    * todo--

---
+++ framework/application/views/_footer.php
@@ -0,0 +1,14 @@
+
+<div id="footer_text">
+	Opinions expressed on this site do not represent those of the
+	<?php echo anchor('http://www.bustudentunion.com/', 'BU Student Union'); ?>.
+</div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-6018923-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
