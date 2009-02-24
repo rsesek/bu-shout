fe2f907848fdf12ee24795e580f0bf0ad18e4019
Robert Sesek <rsesek@bluestatic.org>
2009-02-24 13:37:55 -0500

    Make our Atom feed validate
    * Add the published date/time of entries
    * Fix date formatting

--- framework/application/views/atom_view.php
+++ framework/application/views/atom_view.php
@@ -1,8 +1,9 @@
-<?php echo '<' . '?xml version="1.0" encoding="utf-8"?' . '>' ?>
+<?php echo '<' . '?xml version="1.0" encoding="utf-8"?' . '>' . "\n" ?>
 <feed xmlns="http://www.w3.org/2005/Atom">
 	<title>BU Shout</title>
-	<link href="<?php echo base_url() ?>"/>
-	<updated><?php echo date('Y-m-d\TH:i:s', strtotime($shouts->row()->date)); ?></updated>
+	<link rel="self" href="<?php echo base_url() ?>"/>
+	<id><?php echo base_url() ?></id>
+	<updated><?php echo date('Y-m-d\TH:i:sP', strtotime($shouts->row()->date)); ?></updated>
 	<author>
 		<name>BU Student Union</name>
 		<uri>http://www.bustudentunion.com</uri>
@@ -13,7 +14,8 @@
 		<id><?php echo base_url() ?>shout/detail/<?php echo $shout->id ?></id>
 		<title><?php echo $shout->title ?></title>
 		<content><?php echo $shout->body ?></content>
-		<updated><?php echo date('Y-m-d\TH:i:s', strtotime($shout->lastpost)) ?></updated>
+		<published><?php echo date('Y-m-d\TH:i:sP', strtotime($shout->date)) ?></published>
+		<updated><?php echo date('Y-m-d\TH:i:sP', strtotime($shout->lastpost)) ?></updated>
 	</entry>
 <?php endforeach; ?>
 	
