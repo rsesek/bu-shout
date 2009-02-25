b52edf4aa3bd7bd7684ff066b0b1b41008455fd8
Justin Kaufman <justinkaufman@gmail.com>
2009-02-25 01:20:07 -0500

    Fixed smallest imaginable typo.

--- framework/application/views/_footer.php
+++ framework/application/views/_footer.php
@@ -2,7 +2,7 @@
 <div id="footer_text">
 	<?php echo 'Code and content under ' . license('gpl', 'GPL') . ' and ' . license('cc_by-nc', 'Creative Commons Attribution-NonCommercial') . ' licenses, respectively.'; ?>
 	<br/>
-	Opinions expressed on this site do not necessarily represent those of the <?php echo anchor('http://www.bustudentunion.com/', 'BU Student Union'); ?>
+	Opinions expressed on this site do not necessarily represent those of the <?php echo anchor('http://www.bustudentunion.com/', 'BU Student Union'); ?>.
 </div>
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
